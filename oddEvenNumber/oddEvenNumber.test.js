58f22fc7abad9f93637c0df244a20412be746495
Aishwarya Natarajan <naishwaryaghandhi@gmail.com>
2022-09-21 13:41:17 +0100

    splitaddeven

--- oddEvenNumber/oddEvenNumber.test.js
+++ oddEvenNumber/oddEvenNumber.test.js
@@ -1,3 +1,27 @@
+function splitOddAndEven(numbers) {
+    let even=[];
+    let odd=[];
+    for (const number of numbers) {
+        if (number % 2 == 0) {
+            even.push(number)
+            console.log(even);
+        } else {
+            odd.push(number)
+            console.log(odd);
+        }
+    }
+    let output;
+    return {even:even, odd:odd};
+}
+
+
 describe('oddEvenNumber', () => {
-    expect(true).toBe(false);
+    it('should split odd and even numbers', function () {
+        let numbers = [1,2,3,4]
+        let expectedOutput = {even:[2,4],odd:[1,3]};
+
+        const result = splitOddAndEven(numbers)
+        // expect(splitOddAndEven(numbers)).toBe(splitOddAndEven.even );
+        expect(result).toEqual(expectedOutput);
+    });
 })
